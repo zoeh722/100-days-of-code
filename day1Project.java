d38a03ace891401b8c9ba6291163981a2640ee8b
zoeh722 <zoeh722@vt.edu>
2025-01-23 12:28:48 -0500

    day 1 process

---
+++ day1Project.java
@@ -0,0 +1,14 @@
+/*
+ * This program is the game 2048. The GUI will be in the terminal.
+ * 
+ * Start Date: 1/23/25
+ * 
+ * Author: Zoe Hite
+ * 
+ */
+public class day1Project{ 
+    public static void main(String[] args){
+
+    }
+
+}
